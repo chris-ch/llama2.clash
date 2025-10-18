8739c4802e47aa70d151b40f40d5fe30aeb3e8c7
Christophe <christophe.alexandre@pm.me>
2025-10-18 21:34:55 +0000

    dev

--- project/llama2/LLaMa2/Layers/Attention/MultiHeadAttention.hs
+++ project/llama2/LLaMa2/Layers/Attention/MultiHeadAttention.hs
@@ -14,10 +14,11 @@ import LLaMa2.Layers.Attention.MultiHeadAttention.Internal
   ( computeHeadQ, computeHeadKV )
 import LLaMa2.Helpers.FixedPoint (rmsNormFwFix)
 
+
 projectQKV :: forall dom .
   HiddenClockResetEnable dom
   => Signal dom Bool              -- ^ validIn (enable computation)
-  -> Signal dom Bool              -- ^ readyIn (downstream ready - for protocol)
+  -> Signal dom Bool              -- ^ readyIn (downstream ready)
   -> MultiHeadAttentionComponentQ
   -> Signal dom (Index SequenceLength)
   -> Signal dom (Vec ModelDimension FixedPoint)
@@ -25,28 +26,26 @@ projectQKV :: forall dom .
                   , Vec NumKeyValueHeads (Vec HeadDimension FixedPoint)
                   , Vec NumKeyValueHeads (Vec HeadDimension FixedPoint))
      , Signal dom Bool            -- ^ validOut (all heads done)
-     , Signal dom Bool            -- ^ readyOut (can accept - always True in this design)
+     , Signal dom Bool            -- ^ readyOut (can accept)
      )
 projectQKV validIn readyIn mhaQ seqPosSig xSig =
   (qkvOut, allValid, allReady)
   where
-    -- Normalize input once
     xNorm = rmsNormFwFix <$> xSig <*> pure (rmsAttF mhaQ)
 
-    -- Project all query heads (runs when validIn)
-    qResults = map (\headQ -> computeHeadQ validIn (pure True) headQ seqPosSig xNorm)
+    -- Propagate readyIn to all Q-head multipliers
+    qResults = map (\headQ -> computeHeadQ validIn readyIn headQ seqPosSig xNorm)
                    (headsQ mhaQ)
 
-    -- KV heads (grouped query attention pattern)
     queryHeadsPerKV = natToNum @NumQueryHeads `div` natToNum @NumKeyValueHeads
     kvHeadIndices :: Vec NumKeyValueHeads (Index NumQueryHeads)
     kvHeadIndices = map (\i -> toEnum (fromEnum i * queryHeadsPerKV)) indicesI
 
+    -- Propagate readyIn to all KV-head multipliers
     kvResults = map (\kvIdx -> let headQ = headsQ mhaQ !! kvIdx
-                                in computeHeadKV validIn (pure True) headQ seqPosSig xNorm)
+                                in computeHeadKV validIn readyIn headQ seqPosSig xNorm)
                     kvHeadIndices
 
-    -- Unpack results
     qVecs    = map (\(q, _, _) -> q) qResults
     qValids  = map (\(_, v, _) -> v) qResults
     qReadys  = map (\(_, _, r) -> r) qResults
@@ -56,11 +55,7 @@ projectQKV validIn readyIn mhaQ seqPosSig xSig =
     kvValids = map (\(_, _, v, _) -> v) kvResults
     kvReadys = map (\(_, _, _, r) -> r) kvResults
 
-    -- KEY: All must be valid for output to be valid
     allValid = (and <$> sequenceA qValids) .&&. (and <$> sequenceA kvValids)
-
-    -- All are ready when idle (simplified - they're always ready in this design)
     allReady = (and <$> sequenceA qReadys) .&&. (and <$> sequenceA kvReadys)
 
-    -- Bundle outputs
     qkvOut = bundle (sequenceA qVecs, sequenceA kVecs, sequenceA vVecs)
