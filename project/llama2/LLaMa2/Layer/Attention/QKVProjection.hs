e099168d9bad435a0d984deba47bdc21d94ebbb1
Christophe <christophe.alexandre@pm.me>
2025-11-23 10:14:54 +0000

    dev

--- project/llama2/LLaMa2/Layer/Attention/QKVProjection.hs
+++ project/llama2/LLaMa2/Layer/Attention/QKVProjection.hs
@@ -1,4 +1,4 @@
--- File: LLaMa2/Layer/Attention/QKVProjection.hs (minimal fix)
+-- File: LLaMa2/Layer/Attention/QKVProjection.hs
 module LLaMa2.Layer.Attention.QKVProjection
   ( keyValueHeadProjector
   , qkvProjectionController
@@ -33,30 +33,30 @@ data QHeadDebugInfo dom = QHeadDebugInfo
   , qhRowResult    :: Signal dom FixedPoint
   , qhRowDone      :: Signal dom Bool
   , qhFetchValid   :: Signal dom Bool
-  , qhRowReset     :: Signal dom Bool  -- When is reset active?
-  , qhRowEnable    :: Signal dom Bool  -- When is enable active?
-  , qhAccumValue   :: Signal dom FixedPoint  -- What's in the accumulator?
-  , qhQOut         :: Signal dom (Vec HeadDimension FixedPoint)  -- Current qOut register
-    , qhCurrentRowExp    :: Signal dom Exponent  -- Exponent from hardcoded
-    , qhCurrentRow'Exp   :: Signal dom Exponent  -- Exponent from DRAM
-    , qhCurrentRowMant0  :: Signal dom Mantissa  -- First mantissa hardcoded
-    , qhCurrentRow'Mant0 :: Signal dom Mantissa  -- First mantissa DRAM
+  , qhRowReset     :: Signal dom Bool
+  , qhRowEnable    :: Signal dom Bool
+  , qhAccumValue   :: Signal dom FixedPoint
+  , qhQOut         :: Signal dom (Vec HeadDimension FixedPoint)
+  , qhCurrentRowExp    :: Signal dom Exponent
+  , qhCurrentRow'Exp   :: Signal dom Exponent
+  , qhCurrentRowMant0  :: Signal dom Mantissa
+  , qhCurrentRow'Mant0 :: Signal dom Mantissa
   } deriving (Generic)
 
 --------------------------------------------------------------------------------
--- Q head projector with DDR streaming (FIXED: latch fetched data)
+-- Q head projector with DDR streaming (FIXED: Latch + Handshake)
 --------------------------------------------------------------------------------
 queryHeadProjector :: forall dom.
   HiddenClockResetEnable dom
-  => Slave.AxiSlaveIn dom                     -- ^ DRAM interface
-  -> Index NumLayers                          -- ^ layer index
-  -> Index NumQueryHeads                   -- ^ head index
+  => Slave.AxiSlaveIn dom
+  -> Index NumLayers
+  -> Index NumQueryHeads
   -> Signal dom Bool                          -- ^ inputValid
   -> Signal dom Bool                          -- ^ downStreamReady
   -> Signal dom (Index SequenceLength)
   -> Signal dom (Vec ModelDimension FixedPoint)
-  -> PARAM.DecoderParameters               -- ^ hardcoded (fallback)
-  -> ( Master.AxiMasterOut dom                -- ^ To DRAM
+  -> PARAM.DecoderParameters
+  -> ( Master.AxiMasterOut dom
      , Signal dom (Vec HeadDimension FixedPoint)
      , Signal dom Bool                        -- ^ outputValid
      , Signal dom Bool                        -- ^ readyForInput
@@ -74,30 +74,38 @@ queryHeadProjector dramSlaveIn layerIdx headIdx inputValid downStreamReady stepC
   rowAddr = STREAM.calculateRowAddress STREAM.QMatrix layerIdx headIdx <$> rowIndex
 
   -- Fetch row when starting new row
+  -- FIX 1: We use the fetchTrigger from the FSM to initiate the AXI read
   fetchedWord :: Signal dom (BitVector 512)
   (axiMaster, fetchedWord, fetchValid) = STREAM.axiRowFetcher dramSlaveIn fetchTrigger rowAddr
  
-  -- Parse the LATCHED word
-  currentRow' :: Signal dom (RowI8E ModelDimension)
-  currentRow' = STREAM.parseRow <$> fetchedWord
+  -- Parse the raw word
+  parsedRow :: Signal dom (RowI8E ModelDimension)
+  parsedRow = STREAM.parseRow <$> fetchedWord
 
-  -- Fetch current row from the runtime matrix
+  currentRow' :: Signal dom (RowI8E ModelDimension)
+  currentRow' = register (RowI8E (repeat 0) 0) $
+                  mux (fetchValid .&&. (state .==. pure OPS.MFetching)) 
+                      parsedRow 
+                      currentRow'
+
+  -- Fallback/Reference hardcoded row
   currentRow :: Signal dom (RowI8E ModelDimension)
   currentRow = (!!) (PARAM.wqHeadQ (PARAM.headsQ (PARAM.multiHeadAttention (modelLayers params !! layerIdx)) !! headIdx)) <$> rowIndex
 
-  -- Row processor
+  -- Row processor uses the LATCHED row (currentRow')
   (rowResult, rowDone, colIdx, accValue) = OPS.parallel64RowProcessor rowReset rowEnable currentRow xHat
 
-  -- State machine
+  -- FIX 3: Pass actual fetchValid to FSM.
+  -- This forces the FSM to stay in MFetching until data arrives.
   (state, fetchTrigger, rowReset, rowEnable, outputValid, readyForInput) =
-      OPS.matrixMultiplierStateMachine inputValid downStreamReady rowDone (pure True) rowIndex
+      OPS.matrixMultiplierStateMachine inputValid downStreamReady rowDone fetchValid rowIndex
 
   -- Row index sequencing
-  nextRowIndex = mux (rowDone .&&. (rowIndex ./=. pure maxBound))
-                     (rowIndex + 1)
-                     (mux ((state .==. pure OPS.MDone) .&&. downStreamReady)
-                          (pure 0)
-                          rowIndex)
+  nextRowIndex = mux (state .==. pure OPS.MIdle)
+                      (pure 0)
+                      (mux (rowDone .&&. (rowIndex ./=. pure maxBound))
+                            (rowIndex + 1)
+                            rowIndex)
 
   -- Accumulate results
   qOut = register (repeat 0) nextOutput
@@ -107,15 +115,13 @@ queryHeadProjector dramSlaveIn layerIdx headIdx inputValid downStreamReady stepC
 
   qRoOut = (rotaryEncoder (PARAM.rotaryF (PARAM.headsQ (PARAM.multiHeadAttention (modelLayers params !! layerIdx)) !! headIdx)) <$> stepCount) <*> qOut
 
-  -- Extract first mantissa for debugging
+  -- Debug signals
   firstMantissa = head . rowMantissas <$> currentRow
-
   currentRowExp = rowExponent <$> currentRow
   currentRow'Exp = rowExponent <$> currentRow'
   currentRowMant0 = head . rowMantissas <$> currentRow
   currentRow'Mant0 = head . rowMantissas <$> currentRow'
 
-  -- Package debug info
   debugInfo = QHeadDebugInfo
     { qhRowIndex   = rowIndex
     , qhState      = state
@@ -188,28 +194,21 @@ axiArbiter masters = Master.AxiMasterOut
   where
     arRequests :: Vec n (Signal dom Bool)
     arRequests = map Master.arvalid masters
-
-    -- Priority with fairness: increment when we grant
     lastGranted :: Signal dom (Index n)
     lastGranted = register 0 nextGranted
-    
-    -- Find next requester starting from lastGranted+1
-    -- This ensures fairness while being responsive
     selectedIdx :: Signal dom (Index n)
     selectedIdx = findNextRequester <$> bundle arRequests <*> lastGranted
     
     findNextRequester :: Vec n Bool -> Index n -> Index n
     findNextRequester reqs lastR = 
       let start = if lastR == maxBound then 0 else lastR + 1
-          -- Check each position starting from 'start'
           go i n 
-            | n == (0 :: Int) = lastR  -- No requesters, keep last
-            | reqs !! i = i   -- Found one!
+            | n == (0 :: Int) = lastR 
+            | reqs !! i = i  
             | i == maxBound = go 0 (n-1)
             | otherwise = go (i+1) (n-1)
       in go start (natToNum @n)
     
-    -- Update lastGranted only when we actually grant
     anyRequest = or <$> bundle arRequests
     nextGranted = mux anyRequest selectedIdx lastGranted
 
@@ -217,7 +216,7 @@ axiArbiter masters = Master.AxiMasterOut
     sel f = (!!) <$> bundle (map f masters) <*> selectedIdx
 
 --------------------------------------------------------------------------------
--- QKV projector and controller (unchanged)
+-- QKV projector (unchanged logic)
 --------------------------------------------------------------------------------
 qkvProjector :: forall dom.
   HiddenClockResetEnable dom
@@ -234,33 +233,21 @@ qkvProjector :: forall dom.
                   , Vec NumKeyValueHeads (Vec HeadDimension FixedPoint))
      , Signal dom Bool
      , Signal dom Bool
-     , QHeadDebugInfo dom  -- Debug info from head 0
+     , QHeadDebugInfo dom
      )
 qkvProjector dramSlaveIn layerIdx inputValid downStreamReady seqPos xVec params =
   (axiMasterOut, qkvOut, outputValid, readyForInput, head0Debug)
  where
-
   layerParams = modelLayers params !! layerIdx
   mhaParams = PARAM.multiHeadAttention layerParams
-
   xNorm = rmsNormFwFix <$> xVec <*> pure (PARAM.rmsAttF mhaParams)
 
   qResults = map (qHead params) indicesI
     where
-      qHead :: PARAM.DecoderParameters
-            -> Index NumQueryHeads
-            -> ( Master.AxiMasterOut dom
-              , Signal dom (Vec HeadDimension FixedPoint)
-              , Signal dom Bool
-              , Signal dom Bool
-              , QHeadDebugInfo dom
-              )
       qHead params' headIdx = queryHeadProjector dramSlaveIn layerIdx headIdx
                           inputValid downStreamReady seqPos xNorm params'
 
-  -- Extract debug info from head 0
   head0Debug = head qDebugInfos
-
   qAxiMasters = map (\(axi, _, _, _, _) -> axi) qResults
   qVecs       = map (\(_, q, _, _, _) -> q) qResults
   qValids     = map (\(_, _, v, _, _) -> v) qResults
@@ -275,11 +262,6 @@ qkvProjector dramSlaveIn layerIdx inputValid downStreamReady seqPos xVec params 
 
   kvResults = map kvHead kvHeadIndices
    where
-    kvHead :: Index NumQueryHeads
-           -> ( Signal dom (Vec HeadDimension FixedPoint)
-              , Signal dom (Vec HeadDimension FixedPoint)
-              , Signal dom Bool
-              , Signal dom Bool )
     kvHead qIx =
       let headParams' = PARAM.headsQ mhaParams !! qIx
       in keyValueHeadProjector inputValid downStreamReady seqPos xNorm headParams'
@@ -294,6 +276,9 @@ qkvProjector dramSlaveIn layerIdx inputValid downStreamReady seqPos xVec params 
 
   qkvOut = bundle (sequenceA qVecs, sequenceA kVecs, sequenceA vVecs)
 
+--------------------------------------------------------------------------------
+-- QKV Projection Controller (FIXED: DEADLOCK REMOVED)
+--------------------------------------------------------------------------------
 qkvProjectionController ::
   HiddenClockResetEnable dom
   => Slave.AxiSlaveIn dom
@@ -317,10 +302,15 @@ qkvProjectionController dramSlaveIn layerIdx inputValid downStreamReady input pa
   (enableRaw, outputValid, inReadyRaw) =
     FSM.processingControllerFSM inputValid downStreamReady matVecValid
 
+  -- FIX 4: Do NOT gate enableRaw with projReadyOut_d. 
+  -- We must sustain the Enable signal even when the Projector goes busy (ready=False).
+  enableGated = enableRaw 
+
   (axiMasterOut, result, matVecValid, projReadyOut, debugInfo) =
     qkvProjector dramSlaveIn layerIdx enableGated downStreamReady
                  seqPos input params
 
   projReadyOut_d = register True projReadyOut
-  enableGated    = enableRaw  .&&. projReadyOut_d
+  
+  -- We still use projReadyOut_d to tell the upstream FSM to wait.
   readyForInput  = inReadyRaw .&&. projReadyOut_d
