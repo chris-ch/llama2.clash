425a0952e7c3110f28e5c0c50e2323591b2766f0
Christophe <christophe.alexandre@pm.me>
2026-03-11 14:09:03 +0000

    fixed wrong tokenizer

--- project/app/Main.hs
+++ project/app/Main.hs
@@ -99,13 +99,13 @@ optionsParser =
   Options
     <$> OA.optional (OA.option OA.auto (OA.long "seed" <> OA.help "Seed for debugging"))
     <*> OA.strOption (OA.long "tokenizer-file" <> OA.value
-
-
-
-
-
+#if defined(MODEL_260K)
+      "./data/tok512.bin"
+#elif defined(MODEL_15M)
       "./data/tokenizer.bin"
-
+#else
+      "./data/tokenizer.bin"
+#endif
     <> OA.help "Tokenizer binary file")
     <*> OA.option OA.auto (OA.long "temperature" <> OA.value 0.0 <> OA.metavar "TEMPERATURE" <> OA.help "Temperature")
     <*> OA.option OA.auto (OA.long "steps" <> OA.value 256 <> OA.metavar "STEPS" <> OA.help "Number of steps")
